4852d7fb2df4ca4c282e08cc9991b344374615ab
QuocPhuDEV <phuhq.it@gmail.com>
2019-01-16 09:22:05 +0700

    tuesday_15/jan/2019

--- app/src/main/java/com/example/king/vinamobile/A3_Scan/A3_Scan_Home_Fragment.java
+++ app/src/main/java/com/example/king/vinamobile/A3_Scan/A3_Scan_Home_Fragment.java
@@ -1,11 +1,15 @@
 package com.example.king.vinamobile.A3_Scan;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.king.vinamobile.R;
 
@@ -14,6 +18,9 @@ import com.example.king.vinamobile.R;
  */
 public class A3_Scan_Home_Fragment extends Fragment {
 
+    //region KHAI BÁO BIẾN TOÀN CỤC
+    Button btnScan;
+    //endregion
 
     public A3_Scan_Home_Fragment() {
         // Required empty public constructor
@@ -24,7 +31,34 @@ public class A3_Scan_Home_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_a3__scan__home_, container, false);
+        View view = inflater.inflate(R.layout.fragment_a3__scan__home_, container, false);
+        btnScan = (Button) view.findViewById(R.id.btn_scan);
+        OpenScanFragment();
+        return view;
     }
+
+    //region XỬ LÝ EVENTS
+
+    // Load Fragment
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    // Open Scan Fragment Activity
+    public void OpenScanFragment() {
+        btnScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                A3_Scan_Fragment fragment = new A3_Scan_Fragment();
+                loadFragment(fragment);
+            }
+        });
+    }
+
+    //endregion
 
 }
