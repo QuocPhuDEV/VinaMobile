3c960e1c8ebed1da2f23cea82b9e469f0549405e
QuocPhuDEV <phuhq.it@gmail.com>
2019-02-13 14:20:02 +0700

    wednesday_13022019

--- app/src/main/java/com/example/king/vinamobile/A8_SignIn/A8_SignIn_Fragment.java
+++ app/src/main/java/com/example/king/vinamobile/A8_SignIn/A8_SignIn_Fragment.java
@@ -11,13 +11,19 @@ import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -42,10 +48,12 @@ import de.hdodenhof.circleimageview.CircleImageView;
  */
 public class A8_SignIn_Fragment extends Fragment {
     //region KHAI BÁO BIẾN TOÀN CỤC
-    private TextView DateOfBirth;
+    //private TextView DateOfBirth;
     private Spinner spZone, spCity, spCounty;
     private FloatingActionButton addNewImage;
     private CircleImageView imgAvt;
+    private EditText edFullName, edDateOfBirth, edPhoneNo, edDetailAddress;
+    //private Button btnCancel, btnSignIn;
 
     private DatePickerDialog.OnDateSetListener onDateSetListener;
 
@@ -58,6 +66,11 @@ public class A8_SignIn_Fragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -66,12 +79,17 @@ public class A8_SignIn_Fragment extends Fragment {
             View view = inflater.inflate(R.layout.fragment_a8__sign_in_, container, false);
 
             // ánh xạ
-            DateOfBirth = (TextView) view.findViewById(R.id.a8_ed_date_of_birth);
+            edDateOfBirth = (EditText) view.findViewById(R.id.a8_ed_date_of_birth);
             spZone = (Spinner) view.findViewById(R.id.a8_sp_zone);
             spCity = (Spinner) view.findViewById(R.id.a8_sp_city);
             spCounty = (Spinner) view.findViewById(R.id.a8_sp_county);
             addNewImage = (FloatingActionButton) view.findViewById(R.id.a8_btnAddNewImg);
             imgAvt = (CircleImageView) view.findViewById(R.id.a8_img_avt);
+            edFullName = (EditText) view.findViewById(R.id.a8_ed_full_name);
+            edPhoneNo = (EditText) view.findViewById(R.id.a8_ed_phone_number);
+            edDetailAddress = (EditText) view.findViewById(R.id.a8_ed_address);
+//            btnCancel = (Button) view.findViewById(R.id.a8_btn_cancel);
+//            btnSignIn = (Button) view.findViewById(R.id.a8_btn_submit);
 
 
             // Sự kiệm form load
@@ -89,15 +107,15 @@ public class A8_SignIn_Fragment extends Fragment {
         setDateOfBirth();
         setAddress();
         setImageAvt();
+
     }
     //endregion
 
-    //region XỬ LÝ SỰ KIỆN
-
+    //region XỬ LÝ LOAD FORM
     // khởi tạo date picker ngày tháng
     public void setDateOfBirth() {
         try {
-            DateOfBirth.setOnClickListener(new View.OnClickListener() {
+            edDateOfBirth.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     Calendar calendar = Calendar.getInstance();
@@ -107,7 +125,7 @@ public class A8_SignIn_Fragment extends Fragment {
 
                     DatePickerDialog dialog = new DatePickerDialog(
                             getActivity(),
-                            android.R.style.Theme_DeviceDefault_Dialog_NoActionBar,
+                            android.R.style.Theme_Holo_Light_Dialog_MinWidth,
                             onDateSetListener,
                             year, month, day);
                     dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
@@ -120,7 +138,7 @@ public class A8_SignIn_Fragment extends Fragment {
                 public void onDateSet(DatePicker datePicker, int year, int month, int day) {
                     month = month + 1;
                     String date = day + "/" + month + "/" + year;
-                    DateOfBirth.setText(date);
+                    edDateOfBirth.setText(date);
                 }
             };
         } catch (Exception e) {
@@ -176,7 +194,7 @@ public class A8_SignIn_Fragment extends Fragment {
                     A8_DBHelper dbHelper = new A8_DBHelper(getContext());
                     String TinhTP = spCity.getSelectedItem().toString();
 
-                    List<A8_Cls_City> listQuanHuyen = dbHelper.getAllDataCounty(TinhTP,title_spCounty);
+                    List<A8_Cls_City> listQuanHuyen = dbHelper.getAllDataCounty(TinhTP, title_spCounty);
                     ArrayAdapter<String> adapterCounty = new ArrayAdapter(getContext(), android.R.layout.simple_spinner_item, listQuanHuyen);
                     adapterCounty.setDropDownViewResource(android.R.layout.simple_list_item_single_choice);
                     spCounty.setAdapter(adapterCounty);
@@ -187,11 +205,22 @@ public class A8_SignIn_Fragment extends Fragment {
 
                 }
             });
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    // khởi tạo menu form
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        MenuInflater menuInflater = getActivity().getMenuInflater();
+        menuInflater.inflate(R.menu.a8_menu_complete, menu);
+    }
+    //endregion
+
+    //region XỬ LÝ TÁC VỤ TRONG FORM
     // chọn ảnh từ thư viện
     public void setImageAvt() {
         addNewImage.setOnClickListener(new View.OnClickListener() {
@@ -219,12 +248,62 @@ public class A8_SignIn_Fragment extends Fragment {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == PICK_IMAGE) {
-            Uri selectedImage = data.getData();
-            imgAvt.setImageURI(selectedImage);
+        try {
+            if (requestCode == PICK_IMAGE) {
+                Uri selectedImage = data.getData();
+                imgAvt.setImageURI(selectedImage);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
+    // sự kiện button đăng ký
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        checkEmpty();
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    //endregion
+
+    //region RÀNG BUỘC ĐIỀU KIỆN TRÊN FORM
+
+    // Kiểm tra rỗng
+    public void checkEmpty() {
+        try {
+
+            if (TextUtils.isEmpty(edDetailAddress.getText().toString())) {
+                edDetailAddress.setError(getString(R.string.a8_error_empty_detail_address));
+            }
+
+            if (TextUtils.isEmpty(edDateOfBirth.getText().toString())) {
+                edDateOfBirth.setError(getString(R.string.a8_error_empty_date_of_birth));
+            }else {
+                edDateOfBirth.setError(null);
+            }
+
+            if (TextUtils.isEmpty(edPhoneNo.getText().toString())) {
+                edPhoneNo.setError(getString(R.string.a8_error_empty_phone_number));
+            }
+
+            if (TextUtils.isEmpty(edFullName.getText().toString())) {
+                edFullName.setError(getString(R.string.a8_error_empty_full_name));
+            }
+
+            if (spZone.getSelectedItem().toString().equals(getString(R.string.a8_title_spinner_zone))) {
+                Toast.makeText(getContext(), R.string.a8_error_empty_zone, Toast.LENGTH_SHORT).show();
+            } else if (spCity.getSelectedItem().toString().equals(getString(R.string.a8_title_spinner_city))) {
+                Toast.makeText(getActivity(), R.string.a8_error_empty_city, Toast.LENGTH_SHORT).show();
+            } else if (spCounty.getSelectedItem().toString().equals(getString(R.string.a8_title_spinner_county))) {
+                Toast.makeText(getActivity(), R.string.a8_error_empty_county, Toast.LENGTH_SHORT).show();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     //endregion
 
 }
